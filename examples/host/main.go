bf6f6863e3fbafe2cd77aadbcb738798e79161e2
Namit Chandwani <namitchandwani99@gmail.com>
2021-07-01 22:03:27 +0530

    feat: set up a dummy config system for host CLI

--- examples/host/main.go
+++ examples/host/main.go
@@ -3,30 +3,63 @@ package main
 import (
 	"log"
 
+	"github.com/aerogear/charmil/core/config"
 	"github.com/aerogear/charmil/examples/plugins/adder"
 	"github.com/aerogear/charmil/examples/plugins/date"
 	"github.com/spf13/cobra"
 )
 
-func main() {
-	cmd := &cobra.Command{
+var (
+	h *config.Handler
+
+	f = config.File{
+		Name: "config",
+		Type: "yaml",
+		Path: "./examples/host",
+	}
+
+	cmd = &cobra.Command{
 		Use:          "Host",
 		Short:        "Host CLI for embedding commands",
 		SilenceUsage: true,
 	}
+)
+
+func init() {
+	h = config.New()
+
+	h.InitFile(f)
+
+	err := h.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	h.SetValue("key4", "val4")
 
 	dateCmd, err := date.DateCommand()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	adderCmd, err := adder.AdderCommand()
+	adderCmd, adderCfg, err := adder.AdderCommand()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	cmd.AddCommand(dateCmd)
 	cmd.AddCommand(adderCmd)
+
+	h.SetPluginCfg("adder", adderCfg)
+
+	h.MergePluginCfg()
+
+	err = h.Save()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
