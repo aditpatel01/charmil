54956fc0542b60952960824c6418dececa847eef
Namit Chandwani <namitchandwani99@gmail.com>
2021-07-01 20:34:19 +0530

    feat: add a new example plugin

--- examples/host/main.go
+++ examples/host/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/aerogear/charmil/examples/plugins/adder"
 	"github.com/aerogear/charmil/examples/plugins/date"
 	"github.com/spf13/cobra"
 )
@@ -19,7 +20,13 @@ func main() {
 		log.Fatal(err)
 	}
 
+	adderCmd, err := adder.AdderCommand()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	cmd.AddCommand(dateCmd)
+	cmd.AddCommand(adderCmd)
 
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
